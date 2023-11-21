3275c2e4033ac5376b43579d4dff018445ee053f
brunolopeschagas <brunolopeschagas@gmail.com>
2023-11-21 18:16:28 -0300

    onde nascem os bugs static

--- src/main/java/br/com/programinhas/Main.java
+++ src/main/java/br/com/programinhas/Main.java
@@ -2,21 +2,23 @@ package br.com.programinhas;
 
 public class Main {
     public static void main(String[] args){
-
-        int elementos = 1_000_000_000;
-
-        //tempo inicial
-        long tempoInicial = System.currentTimeMillis();
+        Produto p1 = new Produto();
+        p1.setNome("Pinga");
+        p1.setPreco(1);
+        p1.setQuantidade(3);
         
-        //operacoes
-        System.out.println("ARRAY");
-        System.out.println("INICIO COM " + elementos + " ELEMENTOS EM " + tempoInicial);
+        Produto p2 = new Produto();
+        p2.setNome("Geleia de mocotó");
+        p2.setPreco(10);
+        p2.setQuantidade(10);
+        
+        System.out.println("quantidade p1 " + p1.getQuantidade());
+        System.out.println("quantidade p2 " + p2.getQuantidade());
+        
+        
+        
+        System.out.println("total 1 = " + p1.calcularTotal());
+        System.out.println("total 2 = " + p2.calcularTotal());
 
-        P2Array P2 = new P2Array(elementos);
-
-        //tempo final
-        long tempoFinal = System.currentTimeMillis();
-        System.out.println("FIM COM " + elementos + " ELEMENTOS EM " + tempoFinal);
-        System.out.println("TEMPO TOTAL = " + (tempoFinal - tempoInicial));
     }
 }
---
+++ src/main/java/br/com/programinhas/Produto.java
@@ -0,0 +1,30 @@
+package br.com.programinhas;
+public class Produto {
+
+    private String nome;
+    private double preco;
+    private static int quantidade;
+
+    public double calcularTotal(){
+        return preco * quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
