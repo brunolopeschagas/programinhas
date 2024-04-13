edca899f3b191b5cf3be8af19eeff060271459aa
brunolopeschagas <brunolopeschagas@gmail.com>
2024-04-13 15:36:21 -0300

    string normal test

--- src/main/java/br/com/programinhas/Main.java
+++ src/main/java/br/com/programinhas/Main.java
@@ -2,6 +2,17 @@ package br.com.programinhas;
 
 public class Main {
     public static void main(String[] args){
-        ProdutoRecord p = new ProdutoRecord("asdf", 12);
+
+        long tempoInicial = System.currentTimeMillis();
+        System.out.println();
+
+        String stringNormal = "";
+        for(int i = 0; i < 100_000; i++){
+            stringNormal += i + ", ";
+        }
+
+        System.out.println(stringNormal);
+        long tempoFinal = System.currentTimeMillis();
+        System.out.println("Tempo total: " + (tempoFinal - tempoInicial));
     }
 }
--- src/main/java/br/com/programinhas/ProdutoRecord.java
+++
@@ -1,5 +0,0 @@
-package br.com.programinhas;
-
-public record ProdutoRecord(String nome, double preco) {
-    
-}
